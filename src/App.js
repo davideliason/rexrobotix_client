f1dd8988d61e40e327bca2b0564912ed475fe07a
David Eliason <davethemaker@gmail.com>
2018-09-10 17:59:11 -0700

    add cors

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+	 state = {users: []}
+
+  componentDidMount() {
+    fetch('http://54.202.6.90/api', {method: 'GET', mode: 'cors'})
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+
   render() {
     return (
       <div className="App">
